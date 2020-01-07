69ed670333fb7937f5f26ec7ae8f8d8c678472e5
TheTrveAnthony <hauret.tsi@gmail.com>
2020-01-07 01:30:26 +0100

    long tensors

--- train.py
+++ train.py
@@ -90,7 +90,7 @@ for epoch in range(epochs):
 		outputs = net_model(inputs)
 
 
-		loss = loss_criterion(outputs, masks)
+		loss = loss_criterion(outputs.long(), masks.long())
 		loss.backward()
 
 		optimizer.step()
